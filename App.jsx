d9b7a802a7ed549964814d94a041126b2568bc95
fuzzu05 <fuzailk05@gmail.com>
2025-01-01 15:25:33 +0530

    More Changes and implementation

--- App.jsx
+++ App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Splash from './src/screens/Splash';
+import PLP from './src/screens/PLP';
+import PDP from './src/screens/PDP';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +13,8 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Splash" component={ Splash } options={{ headerShown: false }} />
+        <Stack.Screen name="PLP" component={ PLP } options={{ headerShown: false }} />
+        <Stack.Screen name="PDP" component={ PDP } options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
