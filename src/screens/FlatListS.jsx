f5377da09467a29c6909b815738fc175fe954732
fuzzu05 <fuzailk05@gmail.com>
2025-01-04 00:51:29 +0530

    FlatList Bit change

--- src/screens/FlatListS.jsx
+++ src/screens/FlatListS.jsx
@@ -6,7 +6,7 @@ import ProductItems from '../Components/ProductItems';
 const FlatListS = () => {
 
   const productDisplay = ( { item } ) =>
-  <ProductItems pimage = { item.pimage } name = { item.name } price = { item.price } descrip={ item.description }/>;
+  <ProductItems pimage = { item.pimage } name = { item.name } price = { item.price } descrip={ item.description } />;
 
   return (
     <FlatList 
